196a49453ba441b05f500c37b4a1e4e369410ced
Ruslan Magana Vsevolodovna <ruslan_mv@mail.ru>
2023-10-23 21:55:17 +0200

    Flan Version
    Flan Version

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ def get_text_chunks(text):
 
 # Function to create a vector store
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    #embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     #embeddings = HuggingFaceEmbeddings()
     #vectorstore = FAISS.from_documents(text_chunks, embeddings)
     
@@ -43,8 +43,8 @@ def get_vectorstore(text_chunks):
 
 # Function to create a conversation chain
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    #llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
